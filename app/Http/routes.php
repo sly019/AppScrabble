19ab4ec1e8c86d53cbe7dfb066c5a6022c362a6d
sly019 <deivermora19@gmail.com>
2015-07-18 00:41:12 -0600

    Inicio de proyecto

---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+//Route::get('/', 'WelcomeController@index');
+
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/', 'Auth\AuthController@getLogin');
+Route::get('/home', 'HomeController@index');
+//Route::post('/songs/{id}/enqueue', 'SongsController@enqueue');
+//Route::resource('songs', 'SongsController');
+//Route::resource('users', 'UsersController');
+
+//get( '/Songs' , [
+ //   'as' => 'User' ,
+  //  'uses' => 'SongsController@index'
+//] );
---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('/layouts/layout')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+@section('content')
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+
+        <div class="col-md-2 col-md-offset-2">
+          
+        </div>
+
+      <div class="panel panel-default">
+        <div class="panel-heading">Inicio</div>
+        <div class="panel-body">
+            <img id =""src="http://pad2.whstatic.com/images/thumb/9/99/Improve-Your-Scrabble-Score-Step-4.jpg/670px-Improve-Your-Scrabble-Score-Step-4.jpg">      
+            <button type="submit" class="btn btn-primary" style="margin-right: 15px;">Nueva Partida</button>
+            <button type="submit" class="btn btn-primary" style="margin-right: 15px;">Disponibles</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+
+     
